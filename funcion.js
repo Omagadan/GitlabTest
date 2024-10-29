78b403206234cc0d63bf2ec34578d977a7d4ca6b
Omar Magadan <120671142+Omagadan@users.noreply.github.com>
2024-10-29 17:31:13 -0600

    Create funcion.js

---
+++ funcion.js
@@ -0,0 +1,23 @@
+function generarCadenaAleatoria(longitud) {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let resultado = '';
+    for (let i = 0; i < longitud; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        resultado += caracteres[indiceAleatorio];
+    }
+    return resultado;
+}
+
+function generarCadenasAleatorias(cantidad, longitud) {
+    const cadenas = [];
+    for (let i = 0; i < cantidad; i++) {
+        cadenas.push(generarCadenaAleatoria(longitud));
+    }
+    return cadenas;
+}
+
+const cantidadCadenas = 20;
+const longitudCadena = 10;
+const cadenasAleatorias = generarCadenasAleatorias(cantidadCadenas, longitudCadena);
+
+console.log(cadenasAleatorias);
